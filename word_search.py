9593aae0e3be66f3f01243761d062f28ed68de5d
Sarah Batten <scb0122@auburn.edu>
2026-03-25 16:12:35 -0500

    Updating basic syntax/format based on class example

--- word_search.py
+++ word_search.py
@@ -5,3 +5,12 @@ import re
 if __name__ == "__main__":
     herit_pattern_str = r'\w*herit\w*'
     herit_pattern = re.compile(herit_pattern_str, re.IGNORECASE)
+
+print('Opening origin.txt')
+with open('origin.txt', 'r') as in_stream:
+    print('Opening output.txt')
+    with open('output.txt', 'w') as out_stream:
+        for line in in_stream:
+            line = line.strip()
+            word_list = line.split()
+            word_list
