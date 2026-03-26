592db88fa1b9dc2dc2700e972b9816a63f51514b
Sarah Batten <scb0122@auburn.edu>
2026-03-26 16:07:14 -0500

    Adding docstring and adjusted code to have output match the requested format

--- word_search.py
+++ word_search.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
+# importing the necessary modules for using regular expressions
+
 import re
+
+"""
+The purpose of this script is to iterate over each line of the in_stream, origin.txt to find all each instance where "herit" is used in a word. 
+
+Parameters
+----------
+in_stream: this is a file object that will be searched for the regular expression of interest, "herit".
+
+herit_pattern: a regular expression object that is the target patter of in_stream
+
+Output
+------
+A tuple that contains the object that matches the regular expression. Each time an object that matches the regular expression is found, a tuple is created that has the regular expression match and the line number on which it is found. This is saved in a file called output.txt.
+
+"""
+
 
 if __name__ == "__main__":
     herit_pattern_str = r'\w*herit\w*'
@@ -16,7 +34,7 @@ if __name__ == "__main__":
 
                 for word in word_list:
                     if herit_pattern.search(word):
-                        output = f"Line {line_num}: {word}"
+                        output = f"{line_num}: {word}"
                         print(output)
                         out_stream.write(output + '\n')
     print("Done!")
